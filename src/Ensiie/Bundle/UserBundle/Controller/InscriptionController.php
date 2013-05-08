7ad10e2bc9d17007e71083ada464fcb6760ee54b
gaultier2012 <philigaultier@gmail.com>
2013-05-08 15:55:03 +0200

    Added random password generation

--- src/Ensiie/Bundle/UserBundle/Controller/InscriptionController.php
+++ src/Ensiie/Bundle/UserBundle/Controller/InscriptionController.php
@@ -62,7 +62,7 @@ class InscriptionController extends Controller
 	
 	$logger->info('Génération Password');
 	
-	$mdp = $user->getUsername()."secure";
+	$mdp = $this->Genere_Password(10);
 	$user->setPassword(sha1($mdp));
 	$user->setRoles(array('ROLE_ETU'));
 	$user->setSalt('');
@@ -94,6 +94,19 @@ class InscriptionController extends Controller
       'success'=>'',
     ));
   }
+  public function Genere_Password($size)
+ {
+    // Initialisation des caracteres utilisables
+     $characters = array(0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z");
+     $password = '';
+
+     for($i=0;$i<$size;$i++)
+     {
+            $password .= ($i%2) ? strtoupper($characters[array_rand($characters)]) : $characters[array_rand($characters)];
+     }
+                                
+     return $password;
+ }
 }
 
 ?>
