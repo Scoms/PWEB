9fabedc58c4ab5b16a9acf99282b8d966cd4abec
gaultier2012 <philigaultier@gmail.com>
2013-05-08 16:47:12 +0200

    Added check same password for service change password

--- src/Ensiie/Bundle/UserBundle/Controller/PasswordController.php
+++ src/Ensiie/Bundle/UserBundle/Controller/PasswordController.php
@@ -25,32 +25,40 @@ class PasswordController extends Controller
            
             $user = $this->get('security.context')->getToken()->getUser();
             $form = $this->createFormBuilder($user)
-                         ->add('password','password')
+                         ->add('new_password','password',array(
+                             "mapped" => false,
+                             "label" => "Entrez votre nouveau mot de passe:"
+                         ))
+                         ->add('same_password','password',array(
+                             "mapped" => false,
+                             "label" => "Rentrez de nouveau votre nouveau mot de passe:"
+                         ))
                          ->getForm();
       
             
             if ($request->getMethod() == 'POST')
             {
               $form->bind($request);
-              if ($form->isValid())
-              {
-                $log->info('Valid form');
-                $user->setPassword(sha1($user->getPassword()));
+              
+              $log->info('check same password');
+              if(( $form["new_password"]->getData() != $form["same_password"]->getData()))  
+                    return $this->render('EnsiieUserBundle:Password:index.html.twig',array(
+                      'form' => $form->createView(),
+                      'error'=>'Vous n\'avez pas entré deux fois le même mot de passe',
+                      'success'=>''));
+                  
+                $log->info('Change password');
+                $user->setPassword(sha1($form["new_password"]->getData()));
                 $user->setSalt('');
                 
-                $em->flush();
-              }
-            /*  $log->info('chack password');
-              if($user->getPassword() == '')
-                return $this->render('EnsiieUserBundle:Prof:index.html.twig',array(
-                  'form' => $form->createView(),
-                  'error'=>'Mot de passe invalide',
-                  'success'=>''));*/
-              $log->info('success');
-              return $this->render('EnsiieUserBundle:Password:index.html.twig',array(
-                        'form' => $form->createView(),
-                        'error'=>'',
-                        'success'=>'Votre mot de passe a été changé avec succès.'));
+                $em->flush();              
+             
+                $log->info('success');
+                return $this->render('EnsiieUserBundle:Password:index.html.twig',array(
+                            'form' => $form->createView(),
+                            'error'=>'',
+                            'success'=>'Votre mot de passe a été changé avec succès.'));
+              
             }
             return $this->render('EnsiieUserBundle:Password:index.html.twig', array(
               "form"=>$form->createView(),
