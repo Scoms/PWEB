6336c8a9c6e3fd3947605a2b522efbd759aabfcd
gaultier2012 <philigaultier@gmail.com>
2013-05-08 13:37:43 +0200

    Added depots fixtures + Displaying moyenne (but too many moyennes are displayed FIXME)

--- src/Ensiie/Bundle/MainBundle/Controller/HomeController.php
+++ src/Ensiie/Bundle/MainBundle/Controller/HomeController.php
@@ -8,6 +8,44 @@ class HomeController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('EnsiieMainBundle:Home:index.html.twig');
+      $em = $this->getDoctrine()->getManager();
+      $request = $this->get('request');
+      $log = $this->get('logger');
+      $log->info('HomeController indexAction');
+      
+      $exams = $em->getRepository('EnsiieDataBundle:Examen')->findAll();
+      
+      $log->info('Calcul de la moyenne pour chaque examen');
+      $moyennes = array();
+     
+      foreach($exams as $exam)
+      {
+          $id = $exam->getId();
+          $depots = $em->getRepository('EnsiieDataBundle:Depot')->findBy(array("examen"=>$id));
+          
+          $nb_depots = 0;
+          $moyenne = 0;          
+       
+          $log->info('Calcul de la moyenne d\' un examen a partir des tous les depots attenants');
+          foreach($depots as $depot)
+          {
+              if($depot->getNote()!=null)
+              {
+                $moyenne += $depot->getNote();
+                $nb_depots++;
+              }
+          }
+          if($nb_depots != 0)
+          $moyenne = $moyenne / $nb_depots;
+          
+          $log->info('Ajout de la moyenne calculee dans la liste des moyennes');
+          array_push($moyennes,$moyenne);
+      }
+      
+      
+        
+        return $this->render('EnsiieMainBundle:Home:index.html.twig',
+                array('moyennes' => $moyennes, 'exams' => $exams )
+                );
     }
 }
