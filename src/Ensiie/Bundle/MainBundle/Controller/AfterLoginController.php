402d83f59aab29b9eb13060020291e15dcb2e4a2
Scoms <wieser.laurent@gmail.com>
2013-05-15 09:00:28 +0200

    fix tristan

--- src/Ensiie/Bundle/MainBundle/Controller/AfterLoginController.php
+++ src/Ensiie/Bundle/MainBundle/Controller/AfterLoginController.php
@@ -15,7 +15,7 @@ class AfterLoginController extends Controller
 	if($user->getRoles() == array('ROLE_PROF'))
 	  return $this->redirect($this->generateURL('ensiie_prof',array(),501));
 	if($user->getRoles() == array('ROLE_ETU'))
-	  return $this->redirect($this->generateURL('ensiie_etu',array(),501));
+	  return $this->redirect($this->generateURL('ensiie_etudiant_examen_consult',array(),501));
 	
 	  return $this->redirect($this->generateURL('ensiie_homepage',array(),501));
     }
